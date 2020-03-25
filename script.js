5629d39903b0460997dae7d401fceb018be1c15e
Peachy <44413386+peachy01@users.noreply.github.com>
2020-03-25 10:51:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const myName = 'Temilade Opanuga';
+console.log('My name is' +' ' + myName);
+
+let courses = ['html', 'css', 'javascript'];
+console.log('My courses are' + ' ' + courses);
+
+const course = [];
+for(let i=1; i<= 200; i++){
+    if(i%2==1){
+        course.push(i);
+    } 
+}
+console.log(course);
